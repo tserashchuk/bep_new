b9f87b89d8c37a80dec4126621c36e0203f4c7ad
tserashchuk <tserashchuk.kirill@gmail.com>
2023-11-08 11:22:22 +0300

    ыва

--- bep/processors/base.py
+++ bep/processors/base.py
@@ -2,6 +2,8 @@ from privod.models import Category
 
 def category_processor(request):
     categorys=Category.objects.all()
-    catcount = categorys.count
+    catcount = categorys.count()//2
+    first_menu_data = categorys[:catcount]
+    second_menu_data = categorys[catcount:]
     
-    return ({'catprocessor':categorys,'catcount':catcount})
+    return ({'catprocessor':categorys, 'catcount':catcount, 'first_menu_data':first_menu_data, 'second_menu_data':second_menu_data})
