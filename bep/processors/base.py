2bdcdb8e6cc179beac597cfc3d809500c8336cce
tserashchuk <tserashchuk.kirill@gmail.com>
2023-12-18 00:41:19 +0300

    поправил всякое

--- bep/processors/base.py
+++ bep/processors/base.py
@@ -1,9 +1,9 @@
-from privod.models import Category
+from privod.models import Category, Manufacturer
+from django.db.models.functions import Length
 
 def category_processor(request):
-    categorys=Category.objects.all()
-    catcount = categorys.count()//2
-    first_menu_data = categorys[:catcount]
-    second_menu_data = categorys[catcount:]
-    
-    return ({'catprocessor':categorys, 'catcount':catcount, 'first_menu_data':first_menu_data, 'second_menu_data':second_menu_data})
+    categorys=Category.objects.all().order_by(Length('cat_name').asc())
+    manufacturers = Manufacturer.objects.all().order_by(Length('manufacturer_name').asc())
+    data = [categorys[i:i+3] for i in range(0, len(categorys), 3)]
+    data_manufacturer = [manufacturers[i:i+3] for i in range(0, len(manufacturers), 3)]
+    return ({'catprocessor':categorys, 'manufacturers':manufacturers, 'data':data, 'data_manufacturer':data_manufacturer})
--- privod/views.py
+++ privod/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 from privod.models import *
+from django.db.models.functions import Length
 
 class Home(View):
     def get(self, request):
         products = Product.objects.filter(pin_main=True)
-        categorys = Category.objects.filter(pin_main_first=True)
-        categorys_sec = Category.objects.filter(pin_main_sec=True)
+        categorys = Category.objects.filter(pin_main_first=True).order_by(Length('cat_name'))
+        categorys_sec = Category.objects.filter(pin_main_sec=True).order_by(Length('cat_name'))
         return render(request, 'index.html', {'products':products,'categorys':categorys,'categorys_sec':categorys_sec})
 
 class Categorys(View):
