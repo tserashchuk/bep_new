451250c803849e630ea4d3749df4a15194461715
tserashchuk <tserashchuk.kirill@gmail.com>
2024-07-24 16:16:26 +0300

    чтото невнятное на главной странице

--- bep/processors/base.py
+++ bep/processors/base.py
@@ -1,8 +1,17 @@
-from privod.models import Category, Manufacturer, Producttag
+from privod.models import Category, Manufacturer, Producttag, Product
 from django.db.models.functions import Length
+import random
 
 def category_processor(request):
     categorys=Category.objects.all().order_by(Length('cat_name').asc())
     manufacturers = Manufacturer.objects.all().order_by(Length('manufacturer_name').asc())
     producttags = Producttag.objects.all().order_by(Length('tag_name').asc())
-    return ({'catprocessor_base':categorys, 'manufacturers_base':manufacturers, 'producttags_base':producttags})
+
+
+
+    items = list(Product.objects.all())
+    random_items = random.sample(items, 10)
+    random_item = random.choice(items)
+
+    return ({'catprocessor_base':categorys, 'manufacturers_base':manufacturers, 'producttags_base':producttags, 
+             'random_item':random_items})
